87b9fdcc1cc66a2e983026259aed5c90150e1c52
Scott Clarke <scott.clarke@codethink.co.uk>
2023-06-08 16:25:18 +0100

    Add cleanup for alternate needle versions
    There is now a minion task which will clean up alternate needle files
    created by the WebUI. The minimum retention time of the needle files can
    be set in the config, and defaults to 30 minutes

--- lib/OpenQA/Setup.pm
+++ lib/OpenQA/Setup.pm
@@ -70,6 +70,7 @@ sub read_config ($app) {
             git_auto_clone => 'yes',
             git_auto_update => 'no',
             checkout_needles_sha => 'no',
+            minimum_needle_retention_time => undef,
         },
         scheduler => {
             max_job_scheduled_time => 7,
